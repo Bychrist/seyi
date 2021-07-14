10cea68af723362afe9effb0851e9598a96837eb
Bychrist <ordained07@yahoo.com>
2021-07-14 12:25:23 +0100

    Initial commit

---
+++ semajafrica/database/migrations/2021_06_30_153813_create_role_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoleUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_users', function (Blueprint $table) {
+
+            $table->integer('user_id')->unsigned()->references('id')->on('roles')
+
+                ->onDelete('cascade');
+
+            $table->integer('role_id')->unsigned()->references('id')->on('users')
+
+                ->onDelete('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_users');
+    }
+}
